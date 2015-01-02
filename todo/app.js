54b1186085ae57eaca97ad48dbb2aa6c21a38ef9
Pyunghyuk YOO <yoophi@gmail.com>
2015-01-03 00:02:31 +0900

    session new

--- todo/app.js
+++ todo/app.js
@@ -8,6 +8,8 @@ app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 var file = new nstatic.Server(__dirname + '/public/');
 app.use(flatiron.plugins.http, {
     before: [
+        require('flatware-cookie-parser')(),
+        require('flatware-session')(),
         function (req, res) {
             var found = app.router.dispatch(req, res);
             if (!found) {
@@ -21,5 +23,6 @@ app.use(flatiron.plugins.http, {
 //  this.res.json({ 'hello': 'world' })
 //});
 app.router.path('/users', require('./routes/users'));
+app.router.path('/session', require('./routes/session'));
 
 app.start(3000);
